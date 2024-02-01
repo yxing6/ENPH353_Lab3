27391fc6cc5c2a02f970e5be4e209c9cf4cf934d
yxing6 <yxing6@student.ubc.ca>
2024-01-31 18:50:15 -0800

    modify code structure and added comments

--- node/drive.py
+++ node/drive.py
@@ -13,6 +13,7 @@ class Drive:
 
     def __init__(self):
 
+        # Initialize a ros node
         rospy.init_node('image_subscriber_node', anonymous=True)
 
         # Subscribe to the image topic
@@ -25,75 +26,84 @@ class Drive:
         self.bridge = CvBridge()
 
         # control parameters
-        self.Kp = 0.02 # Proportional gain
-        self.linear_val_max = 0.6
-        self.linear_val_min = 0.2
-        self.error_threshold = 20
-        self.mid_x = 0
-
+        self.Kp = 0.02                  # Proportional gain
+        self.error_threshold = 20       # drive with different linear speed wrt this error_theshold
+        self.linear_val_max = 0.6       # drive fast when error is small 
+        self.linear_val_min = 0.2       # drive slow when error is small 
+        self.mid_x = 0.0                # center of the frame initialized to be 0, updated at each find_middle function call 
+ 
     def image_callback(self, data):
 
+        # process the scribed image from camera in openCV 
+        # convert image message to openCV image 
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         except Exception as e:
             rospy.logerr(e)
             return
+        
+        # Create Twist message and publish to cmd_vel
+        twist_msg = Twist()
+        speed = self.calculate_speed(cv_image)
+        print("speed: ", speed)
+        twist_msg.linear.x = speed[0]
+        twist_msg.angular.z = speed[1]
+        self.cmd_vel_pub.publish(twist_msg)
 
-        angular_error = self.detect_line(cv_image)
 
-        print(angular_error)
+    def calculate_speed(self, img):
+
+        dim_x = img.shape[1]
+        # print(type(dim_x))
+
+
+        # detect_line calculates and modifies the target center
+        self.find_middle(img)
+        # angular error is the different between the center of the frame and targer center
+        angular_error = float(dim_x) / 2 - self.mid_x
+        angular_vel = self.Kp * angular_error
+
+        print(type(angular_vel))
+
         if abs(angular_error) <= self.error_threshold:
             linear_vel = self.linear_val_max
         else:
             linear_vel = self.linear_val_min
-        # linear_vel = 0.2
-        angular_vel = self.Kp * angular_error
+        # linear_vel = 0.5
 
-        # Create Twist message and publish to cmd_vel
-        twist_msg = Twist()
-        twist_msg.linear.x = linear_vel
-        twist_msg.angular.z = angular_vel
-        self.cmd_vel_pub.publish(twist_msg)
+        return linear_vel, angular_vel
 
 
-    def detect_line(self, img):
+    def find_middle(self, img):
+
+        # image processing: 
+        # change the frame to grey scale
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        # blur it so the "road" far away become less easy to detect
+        # kernel_size: Gaussian kernel size
+        # sigma_x: Gaussian kernel standard deviation in X direction
+        # sigma_y: Gaussian kernel standard deviation in Y direction
         kernel_size = 13
-        blur_gray = cv.GaussianBlur(gray,(kernel_size, kernel_size),5, 5)
-        ret, binary = cv.threshold(blur_gray, 70, 255, 0)
-        edges = cv.Canny(binary, 0, 200)
+        sigma_x = 5
+        sigma_y = 5
+        blur_gray = cv.GaussianBlur(gray,(kernel_size, kernel_size), sigma_x, sigma_y)                          # gray scale the image
+
+        # binary it
+        ret, binary = cv.threshold(blur_gray, 70, 255, cv.THRESH_BINARY)
+
         cv.imshow("name", binary)
         cv.waitKey(3)
-        last_row = binary[-1,:]
-        # print(last_row)
-        # print(binary.type())
 
-        # target_row = edges[-30,:]
+        last_row = binary[-1,:]
         print(last_row)
 
-        
-        new_mid_x = self.find_mid_binary(last_row, self.mid_x)
-        self.mid_x = new_mid_x
-
-        mid_frame = len(last_row) / 2
-        error = mid_frame - new_mid_x
-
-        return error
-
-    
-    def find_mid_binary(self, bottom_row, previous_mid):
-        
-        if np.any(bottom_row == 0):
-            bottom_list = bottom_row.tolist()
-            print(bottom_list)
-            first_index = bottom_list.index(0)
-            last_index = len(bottom_list) - 1 - bottom_list[::-1].index(0)
+        if np.any(last_row == 0):
+            last_list = last_row.tolist()
+            first_index = last_list.index(0)
+            last_index = len(last_list) - 1 - last_list[::-1].index(0)
             new_mid = (first_index + last_index)/2
-            print(first_index, new_mid, last_index)
-        else:
-            new_mid = previous_mid
-
-        return new_mid
+            self.mid_x = new_mid
 
 
 def main():
